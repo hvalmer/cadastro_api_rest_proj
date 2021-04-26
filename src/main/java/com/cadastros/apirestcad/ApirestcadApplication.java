3a50e91f5b8cf3802faa89be9512d087aa335f94
hvalmer <hvalmer@gmail.com>
2021-04-26 08:11:04 -0300

    Iniciando a aplicacao

---
+++ src/main/java/com/cadastros/apirestcad/ApirestcadApplication.java
@@ -0,0 +1,13 @@
+package com.cadastros.apirestcad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestcadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestcadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cadastros/apirestcad/ApirestcadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastros.apirestcad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestcadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
