7073d655db8d0f23d10ea0fc2b7e5e3b49d4a7b1
hvalmer <hvalmer@gmail.com>
2021-04-26 12:43:51 -0300

    API REST com Spring Boot - Swagger

--- src/main/java/com/cadastros/apirestcad/resources/CadastroResource.java
+++ src/main/java/com/cadastros/apirestcad/resources/CadastroResource.java
@@ -3,6 +3,7 @@ package com.cadastros.apirestcad.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cadastros.apirestcad.models.Cadastro;
 import com.cadastros.apirestcad.repository.CadastroRepository;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * Crud dos cadastros.
@@ -32,6 +36,8 @@ import com.cadastros.apirestcad.repository.CadastroRepository;
 
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Cadastros")
+@CrossOrigin(origins="*")//liberando todos os domínios para acessar a API
 public class CadastroResource {
 
 	@Autowired
@@ -39,30 +45,35 @@ public class CadastroResource {
 	
 	//Método GET para listar todos os cadastros
 	@GetMapping("/cadastros")
+	@ApiOperation(value="Retorna lista de cadastrados")
 	public List<Cadastro> listaCadastros(){
 		return cadastroRepository.findAll();
 	}
 	
 	//Método GET para listar único cadastro
 	@GetMapping("/cadastro/{id}")
+	@ApiOperation(value="Retorna um cadastro único")
 	public Cadastro listaCadastroUnico(@PathVariable(value="id") long id){
 		return cadastroRepository.findById(id);
 	}
 	
 	//Método POST salva determinado cadastro
 	@PostMapping("/cadastro")
+	@ApiOperation(value="Salva um cadastro")
 	public Cadastro salvaCadastro(@RequestBody Cadastro cadastro) {
 		return cadastroRepository.save(cadastro);
 	}
 	
 	//Método DELETE deleta determinado cadastro
 		@DeleteMapping("/cadastro")
+		@ApiOperation(value="Deleta um cadastro")
 		public void deletaCadastro(@RequestBody Cadastro cadastro) {
 			cadastroRepository.delete(cadastro);
 		}
 		
 	//Método PUT atualiza determinado cadastro
 		@PutMapping("/cadastro")
+		@ApiOperation(value="Atualiza um cadastro")
 		public Cadastro atualizaCadastro(@RequestBody Cadastro cadastro) {
 			return cadastroRepository.save(cadastro);
 		}
